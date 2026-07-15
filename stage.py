a79caed4343db913666d69cbf8b9388616191586
imuta-31ej-code <imuta-31ej@g.bears.ed.jp>
2026-07-15 11:41:04 +0900

    ステージ追加

---
+++ stage.py
@@ -0,0 +1,20 @@
+class Stage:
+
+    def __init__(self):
+
+        # 地面の高さ
+        self.ground_y = 500
+
+    def check_ground(self, player):
+        """
+        地面との衝突判定
+        """
+
+        if player.y + player.height >= self.ground_y:
+
+            player.y = self.ground_y - player.height
+            player.vy = 0
+            player.on_ground = True
+
+        else:
+            player.on_ground = False
