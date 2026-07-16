9cbc5ce9fe17824609e015b35efa56106000b633
imuta-31ej-code <imuta-31ej@g.bears.ed.jp>
2026-07-16 14:14:46 +0900

    game更新

--- javascript/game.js
+++ javascript/game.js
@@ -2,27 +2,40 @@ class Game {
 
     constructor() {
 
-        // Canvas取得
+        // Canvas
         this.canvas = document.getElementById("gameCanvas");
 
-        // 各クラス生成
+        // ゲームオブジェクト
         this.player = new Player();
         this.physics = new Physics();
         this.stage = new Stage();
         this.renderer = new Renderer(this.canvas);
 
-        // FPS設定
+        // 新しく追加
+        this.camera = new Camera();
+        this.input = new Input();
+
+        // FPS
         this.fps = 60;
-        this.dt = 1;
+
+        // 前回の時間
+        this.lastTime = performance.now();
+
+        // ゲーム開始
+        requestAnimationFrame(this.loop.bind(this));
+
     }
 
-    update() {
+    update(dt) {
 
         // 物理更新
-        this.physics.update(this.player, this.dt);
+        this.physics.update(this.player, dt);
 
         // 地面判定
         this.stage.update(this.player);
+
+        // カメラ更新
+        this.camera.update(this.player);
 
     }
 
@@ -35,29 +48,24 @@ class Game {
 
     }
 
-    loop() {
+    loop(currentTime) {
 
-        this.update();
+        const dt = (currentTime - this.lastTime) / 16.666;
+
+        this.lastTime = currentTime;
+
+        this.update(dt);
 
         this.draw();
 
-    }
-
-    start() {
-
-        this.loop();
-
-        setInterval(() => {
-
-            this.loop();
-
-        }, 1000 / this.fps);
+        requestAnimationFrame(this.loop.bind(this));
 
     }
 
 }
 
-// ゲーム開始
-const game = new Game();
+window.onload = () => {
 
-game.start();
+    new Game();
+
+};
