8e3197210e5f2b4afb26b128b5a4e04a7a11558d
imuta-31ej-code <imuta-31ej@g.bears.ed.jp>
2026-07-16 14:11:14 +0900

    input追加

---
+++ javascript/input.js
@@ -0,0 +1,21 @@
+class Input {
+
+    constructor() {
+
+        this.keys = {};
+
+        window.addEventListener("keydown", (e) => {
+            this.keys[e.code] = true;
+        });
+
+        window.addEventListener("keyup", (e) => {
+            this.keys[e.code] = false;
+        });
+
+    }
+
+    isDown(key) {
+        return this.keys[key] === true;
+    }
+
+}
